b143e8bd70e2418ae2653419a5525228e7ade83d
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2017-07-18 15:07:55 -0400

    updated site

---
+++ js/slider.js
@@ -0,0 +1,23 @@
+$(document).ready( function() {
+    $('#myCarousel').carousel({
+		interval:  4000
+	});
+	
+	var clickEvent = false;
+	$('#myCarousel').on('click', '.nav a', function() {
+			clickEvent = true;
+			$('.slider-nav li').removeClass('active');
+			$(this).parent().addClass('active');		
+	}).on('slid.bs.carousel', function(e) {
+		if(!clickEvent) {
+			var count = $('.slider-nav').children().length -1;
+			var current = $('.slider-nav li.active');
+			current.removeClass('active').next().addClass('active');
+			var id = parseInt(current.data('slide-to'));
+			if(count == id) {
+				$('.slider-nav li').first().addClass('active');	
+			}
+		}
+		clickEvent = false;
+	});
+});
